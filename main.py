289bcc34890f7815c2ea24e9b6fb8931c85efc12
Houba28 <Houba28@users.noreply.github.com>
2017-05-30 13:25:54 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 #os.putenv('SDL_FBDEV', '/dev/fb1')
 
 pygame.init()
-
+screen = pygame.display.set_mode((480, 320))
 # set up the colors
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
@@ -13,7 +13,7 @@ GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
  
 # draw on the surface object
-screen.fill(WHITE)
+screen.fill(BLACK)
 
 myfont = pygame.font.SysFont("monospace", 15)
 # set up the window
@@ -21,12 +21,16 @@ screen = pygame.display.set_mode((480, 320))
 label = myfont.render("Some text!", 1, (255,255,0))
 screen.blit(label, (1, 1))
 
-pygame.draw.polygon(screen, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
-
 # run the game loop
 while True:
     for event in pygame.event.get():
             if event.type == QUIT:
                         pygame.quit()
                         sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+            	pos = pygame.mouse.get_pos()
+            	label = myfont.render(str(pos), 1, (255,255,0))
+            	screen.fill(BLACK)
+            	screen.blit(label, (1, 1))
+
     pygame.display.update()
